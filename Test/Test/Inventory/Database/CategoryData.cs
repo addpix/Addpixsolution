6cac9d9c57d6fd56fd348dff31f079a77e41bd78
vaisakhbeypore <vaisakhbeypore@gmail.com>
2017-08-02 15:22:01 +0530

    new files in inventory

---
+++ Test/Test/Inventory/Database/CategoryData.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Test.Inventory.Database
+{
+    class CategoryData
+    {
+        public string CatNo { get; set; }
+        public string Category { get; set; }
+        public string Delete { get; set; }
+        SqlCommand Cmd;
+        SqlConnection Con;
+        SqlTransaction Trans;
+        string Result = "";
+
+        public void FnConn()
+        {
+            Con = new SqlConnection(ConfigurationManager.ConnectionStrings["newConnectionString"].ConnectionString);
+            Con.Open();
+            Trans = Con.BeginTransaction();
+        }
+
+        public DataTable FillData()
+        {
+            try
+            {
+                DataTable dtReturnTable = new DataTable();
+
+                Cmd = new SqlCommand("spCategory", Con, Trans);
+                Cmd.CommandType = CommandType.StoredProcedure;
+                Cmd.Parameters.AddWithValue("@OPERATION", "S");
+                SqlDataAdapter adp = new SqlDataAdapter(Cmd);
+
+                adp.Fill(dtReturnTable);
+                return dtReturnTable;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return new DataTable();
+            }
+        }
+        public void fnTransactionData()
+        {
+            Cmd = new SqlCommand("spCategory", Con, Trans);
+
+            Cmd.CommandType = CommandType.StoredProcedure;
+            Cmd.Parameters.AddWithValue("@OPERATION", "I");
+            Cmd.Parameters.AddWithValue("@CATEGORY", Category);
+            Cmd.ExecuteNonQuery();
+        }
+        public string FnTrans()
+        {
+            try
+            {
+                Trans.Commit();
+                Result = "Success";
+                return Result;
+            }
+            catch (SqlException sqlEx)
+            {
+                Trans.Rollback();
+                Result = "Error" + sqlEx.Message;
+                return Result;
+            }
+            finally
+            {
+                Con.Close();
+                Con.Dispose();
+            }
+        }
+    }
+}
---
+++ Test/Test/Inventory/Database/NewBrandData.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Test.Inventory.Database
+{
+    class NewBrandData
+    {
+        public string Brand { get; set; }
+        public string Vendor { get; set; }
+        string Result = "";
+
+        SqlCommand Cmd;
+        SqlConnection Con;
+        SqlTransaction Trans;
+
+        public void FnConn()
+        {
+            Con = new SqlConnection(ConfigurationManager.ConnectionStrings["newConnectionString"].ConnectionString);
+            Con.Open();
+            Trans = Con.BeginTransaction();
+        }
+        public DataTable FillData()
+        {
+            try
+            {
+                DataTable dtReturnTable = new DataTable();
+
+                Cmd = new SqlCommand("spBrand", Con, Trans);
+                Cmd.CommandType = CommandType.StoredProcedure;
+                Cmd.Parameters.AddWithValue("@OPERATION", "S");
+                SqlDataAdapter adp = new SqlDataAdapter(Cmd);
+
+                adp.Fill(dtReturnTable);
+                return dtReturnTable;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return new DataTable();
+            }
+        }
+        public void fnTransactionData()
+        {
+            Cmd = new SqlCommand("spBrand", Con, Trans);
+            Cmd.CommandType = CommandType.StoredProcedure;
+
+            Cmd.Parameters.AddWithValue("@CATEGORY", Brand);
+            Cmd.Parameters.AddWithValue("@VENDOR", Vendor);
+            Cmd.ExecuteNonQuery();
+        }
+
+        public string FnTrans()
+        {
+            try
+            {
+                Trans.Commit();
+                Result = "Success";
+                return Result;
+            }
+            catch (SqlException sqlEx)
+            {
+                Trans.Rollback();
+                Result = "Error" + sqlEx.Message;
+                return Result;
+            }
+
+            finally
+            {
+                Con.Close();
+                Con.Dispose();
+            }
+        }
+    }
+}
